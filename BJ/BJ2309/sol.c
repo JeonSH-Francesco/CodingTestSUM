f2f05ad2ed564872e6a2b2c54cb39a5404eb6d19
Ares <112309895+JeonSH-Francesco@users.noreply.github.com>
2024-10-27 13:03:50 +0900

    Create sol.c

---
+++ BJ/BJ2309/sol.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define N 9
+#define TARGET_SUM 100
+
+int main() {
+	int heights[N];
+	int sum = 0;
+	int found = 0;
+
+	//키 입력 및 전체 합 계산
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &heights[i]);
+		sum += heights[i];
+	}
+
+	// 일곱 난쟁이 찾기
+	for (int i = 0; i < N - 1; i++) {
+		if (found) break;  // 두 난쟁이를 찾으면 루프 종료
+		for (int j = i + 1; j < N; j++) {
+			if (sum - heights[i] - heights[j] == TARGET_SUM) {
+				heights[i] = heights[j] = 0;  // 두 난쟁이 제거
+				found = 1;  // 플래그 설정
+				break;  // 내부 루프 종료
+			}
+		}
+	}
+
+
+	//오름 차순 정렬 및 출력
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++) {
+			if (heights[i] > heights[j]) {
+				int temp = heights[i];
+				heights[i] = heights[j];
+				heights[j] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		if (heights[i] != 0) {
+			printf("%d\n",heights[i]);
+		}
+	}
+	return 0;
+}
