474a5147eedfec1a84919afd3973b9f3f700bcae
Ares <112309895+JeonSH-Francesco@users.noreply.github.com>
2025-04-10 19:00:51 +0900

    Update sol.py

--- CT/고대 문명 유적 탐사/sol.py
+++ CT/고대 문명 유적 탐사/sol.py
@@ -1,11 +1,9 @@
-K, M = map(int,input().split()) #K는 턴, M은 유물개수
-arr=[list(map(int,input().split())) for _ in range(5)] #5x5 입력 -> 유적지 크기
+K, M = map(int,input().split()) #K는 턴 수, M은 추가로 받는 유물의 수 입력
+arr=[list(map(int,input().split())) for _ in range(5)] #5x5의 유적지 입력
+lst= list(map(int,input().split())) #추가로 받는 유적을 담기 위한 리스트
+ans=[] #답
 
-lst = list(map(int,input().split())) #유물이 사라진 자리를 채우기 위해 사용할 유물들
-
-ans = [] #유물의 최대 가치의 총 합을 반환하기 위한 리스트
-
-
+#90도 시계방향 회전
 def rotate(arr,si,sj):
     narr=[x[:] for x in arr]
     for i in range(3):
@@ -13,16 +11,18 @@ def rotate(arr,si,sj):
             narr[si+i][sj+j]=arr[si+3-j-1][sj+i]
     return narr
 
+#bfs를 통해 유물이 있는 경우 탐색
 def bfs(arr,v,si,sj,clr):
     q = []
-    sset = set()
-    cnt = 0
+    sset = set() #유물 위치를 저장하기 위한 변수
+    cnt = 0 #유물 개수 세기 위한 변수
 
     q.append((si,sj))
+    
     v[si][sj]=1
     sset.add((si,sj))
     cnt+=1
-
+    
     while q:
         ci,cj = q.pop(0)
         # 네방향, 범위내, 미방문, 조건: 같은 값이면
@@ -34,15 +34,15 @@ def bfs(arr,v,si,sj,clr):
                 sset.add((ni,nj))
                 cnt+=1
 
-    if cnt>=3:      # 유물이면: cnt 리턴 + clr==1이면 0으로 clear
+    if cnt>=3:      # 유물로 인정되었고, 제거 모드인 경우(3개 이상인 경우)
         if clr==1:  # 0으로 초기화
             for i,j in sset:
-                arr[i][j]=0
-        return cnt
-    else:           # 3개 미만이면 0리턴
-        return 0
-    
-
+                arr[i][j]=0 #해당 좌표들을 0으로 초기화(제거)
+        return cnt #유물이면: cnt 리턴
+    else:           
+        return 0 # 3개 미만이면 0리턴
+     
+#유물이 얼마나 사라지는지 확인
 def count_clear(arr, clr):  # clr==1인 경우 3개이상값들을 0으로 clear
     v = [[0]*5 for _ in range(5)]
     cnt = 0
@@ -53,8 +53,6 @@ def count_clear(arr, clr):  # clr==1인 경우 3개이상값들을 0으로 clear
                 t = bfs(arr,v,i,j,clr)
                 cnt+=t
     return cnt
-
-
 
 for _ in range(K):
     #[1]탐사 진행
@@ -72,19 +70,19 @@ for _ in range(K):
                 for _ in range(rot):
                     narr=rotate(narr,si,sj)
                     
-                #유무개수 카운트-> 유물 제거
+                #가장 많이 나올 수 있는 유물 개수를 cnt해서 선택하기 위한 작업
                 t=count_clear(narr,0)
                 if mx_cnt<t:
                     mx_cnt=t
-                    marr=narr #최대 개수
+                    marr=narr #유물을 최대 개수만큼 할 수 있는 marr를 narr로 갱신
                     
     #유물이 없는 경우 턴 즉시종료
-    if mx_cnt==0:
-        break
+    if mx_cnt==0: #유물이 더 이상 안 나오는 경우
+        break #바로 종료
         
     #[2] 연쇄획득
-    cnt=0
-    arr=marr 
+    cnt=0 #실제 턴에서 유물이 얼마나 사라졌는지 계산하기 위한 변수
+    arr=marr #최대의 유물을 획득할 수 있는 배열로 갱신
     while True:
         t=count_clear(arr,1)
         if t==0:
@@ -92,6 +90,7 @@ for _ in range(K):
         cnt +=t #발굴한 유물 개수 누적
         
         #arr의 0값인 부분 리스트에서 순서대로 추가
+        #문제 조건에 따라 유물을 추가할 경우 열을 기준으로 행 수가 큰 경우가 우선권을 가지므로
         for j in range(5):
             for i in range(4,-1,-1):
                 if arr[i][j]==0:
@@ -109,5 +108,6 @@ print(*ans)
 5 4 3 2 7
 3 2 3 5 2 4 6 1 3 2 5 6 2 1 5 6 7 1 2 3
 17
+
 
 '''
