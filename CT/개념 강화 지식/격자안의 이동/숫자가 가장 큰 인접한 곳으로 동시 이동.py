724ee7b5ed08bb5f9da6c8648860ba15ed442aa3
Ares <112309895+JeonSH-Francesco@users.noreply.github.com>
2025-07-17 23:10:35 +0900

    Create 숫자가 가장 큰 인접한 곳으로 동시 이동.py

---
+++ CT/개념 강화 지식/격자안의 이동/숫자가 가장 큰 인접한 곳으로 동시 이동.py
@@ -0,0 +1,39 @@
+n, m, t = map(int, input().split())  # 격자 크기, 구슬 수, 시간
+a = [list(map(int, input().split())) for _ in range(n)]  # 격자 정보
+
+# 구슬 초기 위치: 1-based 입력 → 0-based 변환
+marbles = [tuple(map(lambda x: int(x) - 1, input().split())) for _ in range(m)]
+
+# 방향: 상, 하, 좌, 우
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+for _ in range(t):
+    next_marbles=[]
+    
+    for r,c in marbles:
+        max_value=-1
+        nr, nc = r,c #이동하지 않을 수도 있으니 초기값은 자기 자리
+        
+        for d in range(4):
+            tr = r+dr[d]
+            tc = c+dc[d]
+            
+            if 0<=tr<n and 0<=tc<n and a[tr][tc]>max_value:
+                max_value= a[tr][tc]
+                nr, nc = tr, tc
+        next_marbles.append((nr,nc))
+        
+    #위치별 등장 횟수 세기
+    count={}
+    for pos in next_marbles:
+        if pos in count:
+            count[pos]+=1
+        else:
+            count[pos]=1
+    #충돌 없는 구슬만 남김
+    new_marbles=[pos for pos in next_marbles if count[pos]==1]
+    marbles=new_marbles
+    
+#최종 살아남은 구슬 수 출력
+print(len(marbles))
