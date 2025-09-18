bbc5ed87c5ea06660437a6560fd9e25b7de3c252
Ares <112309895+JeonSH-Francesco@users.noreply.github.com>
2025-09-18 22:38:29 +0900

    Create sol.py

---
+++ CT/개념 강화 지식/정수 N개의 합-3/sol.py
@@ -0,0 +1,41 @@
+n, k = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+# 2차원 누적합 ps[i][j] = (1,1) ~ (i,j) 구간 합 
+ps = [[0] * (n + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    row_sum = 0
+    for j in range(1, n + 1):
+        row_sum += arr[i - 1][j - 1]
+        ps[i][j] = ps[i - 1][j] + row_sum
+
+# 모든 K×K 정사각형의 합을 확인하며 최댓값 갱신
+ans = 0
+for i in range(k, n + 1):
+    for j in range(k, n + 1):
+        total = ps[i][j] - ps[i - k][j] - ps[i][j - k] + ps[i - k][j - k]
+        ans = max(ans, total)
+
+print(ans)
+
+'''
+입력
+3 1
+1 2 3
+9 8 8
+6 8 8
+나의 출력
+9
+정답
+9
+----------------------
+입력
+3 2
+1 2 3
+9 8 8
+6 8 8
+나의 출력
+32
+정답
+32
+'''
