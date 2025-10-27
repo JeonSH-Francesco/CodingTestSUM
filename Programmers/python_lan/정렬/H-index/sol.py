8a82381a55f7ccc7d6e395a2b5c315f12f8aa47d
Ares <112309895+JeonSH-Francesco@users.noreply.github.com>
2025-10-27 22:44:16 +0900

    Update sol.py

--- Programmers/python_lan/정렬/H-index/sol.py
+++ Programmers/python_lan/정렬/H-index/sol.py
@@ -1,6 +1,8 @@
 def solution(citations):
-    citations.sort(reverse=True)  # 인용 횟수를 내림차순 정렬
+    citations.sort(reverse=True)  # 인용 횟수를 내림차순 정렬하여 인덱스=상위 논문 수
+    # i+1 = 상위 i+1 편 논문, c=i번째 논문의 인용 횟수
     for i, c in enumerate(citations):
+        #상위 h편 논문이 h회 이상 인용되고, 나머지 논문은 h회 이하 인용되면 H-index이므로
         if i + 1 > c:  # i는 0부터 시작하므로 i+1편의 논문이 i+1회 이상 인용된 건지 확인
             return i
     return len(citations)
